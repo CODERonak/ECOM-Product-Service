1031b7fea12b7360a6744b78677dae95d5810dfb
r9988.2588 <r9988.2588@gmail.com>
2026-06-03 06:09:33 +0000

    add @Document annotation

--- src/main/java/com/ecom/productservice/model/entity/Product.java
+++ src/main/java/com/ecom/productservice/model/entity/Product.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ecom.productservice.model.enums.ProductCategory;
 import com.ecom.productservice.model.enums.ProductStatus;
@@ -16,6 +17,7 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "products")
 public class Product {
     @Id
     private String id;
