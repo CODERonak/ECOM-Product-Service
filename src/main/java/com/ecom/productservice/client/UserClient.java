98feb79437f1149592222b9c29f4e5781af96085
r9988.2588 <r9988.2588@gmail.com>
2026-06-09 08:20:59 +0000

    refactor

--- src/main/java/com/ecom/productservice/client/UserClient.java
+++ src/main/java/com/ecom/productservice/client/UserClient.java
@@ -2,17 +2,16 @@ package com.ecom.productservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "${user.service.url}")
 public interface UserClient {
 
-    // Mirrors POST /users/validate in User Service
-    @PostMapping("api/auth/validate")
+    // Mirrors POST /auth/validate in User Service
+    @GetMapping("api/auth/validate")
     Boolean validateToken(@RequestParam("token") String token);
 
-    // Mirrors GET /users/email in User Service
+    // Mirrors GET /auth/email in User Service
     @GetMapping("api/auth/email")
     String extractEmailFromToken(@RequestParam("token") String token);
 }
--- src/main/java/com/ecom/productservice/security/SecurityConfig.java
+++ src/main/java/com/ecom/productservice/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.ecom.productservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,7 +20,7 @@ public class SecurityConfig {
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests((requests) -> requests
                                                 .requestMatchers(
-                                                                "/api/products/",
+                                                                "/api/products",
                                                                 "/api/products/category/{category}",
                                                                 "/api/products/all",
                                                                 "/api/products/{id}",
@@ -31,7 +30,7 @@ public class SecurityConfig {
                                                                 "/swagger-ui.html")
                                                 .permitAll()
                                                 .anyRequest().authenticated())
-                                .httpBasic(Customizer.withDefaults())
+                                // .httpBasic(Customizer.withDefaults())
                                 .formLogin(formLogin -> formLogin.disable())
                                 .sessionManagement(session -> session
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
